1c199fd7858a3429362e4e6fd5271d1ffc24f011
Muhammad Al-Sadaka <muhammadsadka95@gmail.com>
2026-08-15 15:00:25 +0300

    Add Users/ Properties/ TempClass.cs

---
+++ MediManage/Users/frmLogin.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace MediManage
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.WorkingArea = new System.Windows.Forms.Panel();
+            this.airForm1 = new ReaLTaiizor.Forms.AirForm();
+            this.nightPanel1 = new ReaLTaiizor.Controls.NightPanel();
+            this.crownLabel1 = new ReaLTaiizor.Controls.CrownLabel();
+            this.bigLabel3 = new ReaLTaiizor.Controls.BigLabel();
+            this.bigLabel2 = new ReaLTaiizor.Controls.BigLabel();
+            this.bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
+            this.chkRememberMe = new System.Windows.Forms.CheckBox();
+            this.tbPassword = new ReaLTaiizor.Controls.CyberTextBox();
+            this.tbUserName = new ReaLTaiizor.Controls.CyberTextBox();
+            this.btnLogin = new ReaLTaiizor.Controls.CyberButton();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.airForm1.SuspendLayout();
+            this.nightPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WorkingArea
+            // 
+            this.WorkingArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.WorkingArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WorkingArea.Location = new System.Drawing.Point(0, 39);
+            this.WorkingArea.Name = "WorkingArea";
+            this.WorkingArea.Size = new System.Drawing.Size(1099, 437);
+            this.WorkingArea.TabIndex = 0;
+            // 
+            // airForm1
+            // 
+            this.airForm1.BackColor = System.Drawing.Color.White;
+            this.airForm1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.airForm1.Controls.Add(this.nightPanel1);
+            this.airForm1.Customization = "AAAA/1paWv9ycnL/";
+            this.airForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.airForm1.Font = new System.Drawing.Font("Segoe UI Emoji", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.airForm1.Image = null;
+            this.airForm1.Location = new System.Drawing.Point(0, 0);
+            this.airForm1.MinimumSize = new System.Drawing.Size(112, 35);
+            this.airForm1.Movable = true;
+            this.airForm1.Name = "airForm1";
+            this.airForm1.NoRounding = false;
+            this.airForm1.Sizable = true;
+            this.airForm1.Size = new System.Drawing.Size(1072, 518);
+            this.airForm1.SmartBounds = true;
+            this.airForm1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.airForm1.TabIndex = 0;
+            this.airForm1.Text = "MediManage";
+            this.airForm1.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.airForm1.Transparent = false;
+            // 
+            // nightPanel1
+            // 
+            this.nightPanel1.BackgroundImage = global::MediManage.Properties.Resources._1779126696933;
+            this.nightPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nightPanel1.Controls.Add(this.crownLabel1);
+            this.nightPanel1.Controls.Add(this.bigLabel3);
+            this.nightPanel1.Controls.Add(this.bigLabel2);
+            this.nightPanel1.Controls.Add(this.bigLabel1);
+            this.nightPanel1.Controls.Add(this.chkRememberMe);
+            this.nightPanel1.Controls.Add(this.tbPassword);
+            this.nightPanel1.Controls.Add(this.tbUserName);
+            this.nightPanel1.Controls.Add(this.btnLogin);
+            this.nightPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.nightPanel1.LeftSideColor = System.Drawing.Color.White;
+            this.nightPanel1.Location = new System.Drawing.Point(0, 36);
+            this.nightPanel1.Name = "nightPanel1";
+            this.nightPanel1.RightSideColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(61)))));
+            this.nightPanel1.Side = ReaLTaiizor.Controls.NightPanel.PanelSide.Left;
+            this.nightPanel1.Size = new System.Drawing.Size(1072, 482);
+            this.nightPanel1.TabIndex = 0;
+            // 
+            // crownLabel1
+            // 
+            this.crownLabel1.AutoSize = true;
+            this.crownLabel1.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crownLabel1.ForeColor = System.Drawing.Color.Black;
+            this.crownLabel1.Location = new System.Drawing.Point(662, 77);
+            this.crownLabel1.Name = "crownLabel1";
+            this.crownLabel1.Size = new System.Drawing.Size(315, 38);
+            this.crownLabel1.TabIndex = 8;
+            this.crownLabel1.Text = "  Login to Your Account";
+            // 
+            // bigLabel3
+            // 
+            this.bigLabel3.AutoSize = true;
+            this.bigLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.bigLabel3.Font = new System.Drawing.Font("Segoe UI Semibold", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bigLabel3.ForeColor = System.Drawing.Color.White;
+            this.bigLabel3.Location = new System.Drawing.Point(60, 96);
+            this.bigLabel3.Name = "bigLabel3";
+            this.bigLabel3.Size = new System.Drawing.Size(78, 57);
+            this.bigLabel3.TabIndex = 7;
+            this.bigLabel3.Text = "TO";
+            // 
+            // bigLabel2
+            // 
+            this.bigLabel2.AutoSize = true;
+            this.bigLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.bigLabel2.Font = new System.Drawing.Font("Segoe UI Semibold", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bigLabel2.ForeColor = System.Drawing.Color.White;
+            this.bigLabel2.Location = new System.Drawing.Point(47, 153);
+            this.bigLabel2.Name = "bigLabel2";
+            this.bigLabel2.Size = new System.Drawing.Size(277, 57);
+            this.bigLabel2.TabIndex = 6;
+            this.bigLabel2.Text = "MediManage";
+            // 
+            // bigLabel1
+            // 
+            this.bigLabel1.AutoSize = true;
+            this.bigLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.bigLabel1.Font = new System.Drawing.Font("Segoe UI Semibold", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bigLabel1.ForeColor = System.Drawing.Color.White;
+            this.bigLabel1.Location = new System.Drawing.Point(12, 39);
+            this.bigLabel1.Name = "bigLabel1";
+            this.bigLabel1.Size = new System.Drawing.Size(226, 57);
+            this.bigLabel1.TabIndex = 5;
+            this.bigLabel1.Text = "WELCOME";
+            // 
+            // chkRememberMe
+            // 
+            this.chkRememberMe.AutoSize = true;
+            this.chkRememberMe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkRememberMe.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkRememberMe.ForeColor = System.Drawing.Color.Black;
+            this.chkRememberMe.Location = new System.Drawing.Point(660, 278);
+            this.chkRememberMe.Name = "chkRememberMe";
+            this.chkRememberMe.Size = new System.Drawing.Size(122, 24);
+            this.chkRememberMe.TabIndex = 4;
+            this.chkRememberMe.Text = "Remember me";
+            this.chkRememberMe.UseVisualStyleBackColor = true;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Alpha = 20;
+            this.tbPassword.BackColor = System.Drawing.Color.Transparent;
+            this.tbPassword.Background_WidthPen = 3F;
+            this.tbPassword.BackgroundPen = true;
+            this.tbPassword.ColorBackground = System.Drawing.Color.White;
+            this.tbPassword.ColorBackground_Pen = System.Drawing.Color.DarkRed;
+            this.tbPassword.ColorLighting = System.Drawing.Color.Lime;
+            this.tbPassword.ColorPen_1 = System.Drawing.Color.Yellow;
+            this.tbPassword.ColorPen_2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.tbPassword.CyberTextBoxStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            this.tbPassword.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F);
+            this.tbPassword.ForeColor = System.Drawing.Color.DimGray;
+            this.tbPassword.Lighting = false;
+            this.tbPassword.LinearGradientPen = false;
+            this.tbPassword.Location = new System.Drawing.Point(648, 209);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.PenWidth = 15;
+            this.tbPassword.RGB = false;
+            this.tbPassword.Rounding = true;
+            this.tbPassword.RoundingInt = 60;
+            this.tbPassword.Size = new System.Drawing.Size(322, 50);
+            this.tbPassword.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.tbPassword.TabIndex = 3;
+            this.tbPassword.Tag = "Cyber";
+            this.tbPassword.TextButton = "Password";
+            this.tbPassword.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.tbPassword.Timer_RGB = 300;
+            this.tbPassword.Enter += new System.EventHandler(this.tbPassword_Enter);
+            this.tbPassword.Leave += new System.EventHandler(this.tbPassword_Leave);
+            this.tbPassword.Validating += new System.ComponentModel.CancelEventHandler(this.tbPassword_Validating);
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Alpha = 20;
+            this.tbUserName.BackColor = System.Drawing.Color.Transparent;
+            this.tbUserName.Background_WidthPen = 3F;
+            this.tbUserName.BackgroundPen = true;
+            this.tbUserName.ColorBackground = System.Drawing.Color.White;
+            this.tbUserName.ColorBackground_Pen = System.Drawing.Color.DarkRed;
+            this.tbUserName.ColorLighting = System.Drawing.Color.Lime;
+            this.tbUserName.ColorPen_1 = System.Drawing.Color.Yellow;
+            this.tbUserName.ColorPen_2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.tbUserName.CyberTextBoxStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            this.tbUserName.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F);
+            this.tbUserName.ForeColor = System.Drawing.Color.DimGray;
+            this.tbUserName.Lighting = false;
+            this.tbUserName.LinearGradientPen = false;
+            this.tbUserName.Location = new System.Drawing.Point(648, 140);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.PenWidth = 15;
+            this.tbUserName.RGB = false;
+            this.tbUserName.Rounding = true;
+            this.tbUserName.RoundingInt = 60;
+            this.tbUserName.Size = new System.Drawing.Size(322, 50);
+            this.tbUserName.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.tbUserName.TabIndex = 2;
+            this.tbUserName.Tag = "Cyber";
+            this.tbUserName.TextButton = "User name";
+            this.tbUserName.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.tbUserName.Timer_RGB = 300;
+            this.tbUserName.Enter += new System.EventHandler(this.tbUserName_Enter);
+            this.tbUserName.Leave += new System.EventHandler(this.tbUserName_Leave);
+            this.tbUserName.Validating += new System.ComponentModel.CancelEventHandler(this.tbUserName_Validating);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Alpha = 20;
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.Background = true;
+            this.btnLogin.Background_WidthPen = 4F;
+            this.btnLogin.BackgroundPen = true;
+            this.btnLogin.ColorBackground = System.Drawing.Color.DarkRed;
+            this.btnLogin.ColorBackground_1 = System.Drawing.Color.Red;
+            this.btnLogin.ColorBackground_2 = System.Drawing.Color.Red;
+            this.btnLogin.ColorBackground_Pen = System.Drawing.Color.Red;
+            this.btnLogin.ColorLighting = System.Drawing.Color.Red;
+            this.btnLogin.ColorPen_1 = System.Drawing.Color.Red;
+            this.btnLogin.ColorPen_2 = System.Drawing.Color.Red;
+            this.btnLogin.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            this.btnLogin.Effect_1 = true;
+            this.btnLogin.Effect_1_ColorBackground = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(200)))), ((int)(((byte)(238)))));
+            this.btnLogin.Effect_1_Transparency = 25;
+            this.btnLogin.Effect_2 = true;
+            this.btnLogin.Effect_2_ColorBackground = System.Drawing.Color.White;
+            this.btnLogin.Effect_2_Transparency = 20;
+            this.btnLogin.Font = new System.Drawing.Font("Franklin Gothic Medium", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.btnLogin.Lighting = false;
+            this.btnLogin.LinearGradient_Background = false;
+            this.btnLogin.LinearGradientPen = false;
+            this.btnLogin.Location = new System.Drawing.Point(639, 321);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.PenWidth = 15;
+            this.btnLogin.Rounding = true;
+            this.btnLogin.RoundingInt = 70;
+            this.btnLogin.Size = new System.Drawing.Size(340, 57);
+            this.btnLogin.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Tag = "Cyber";
+            this.btnLogin.TextButton = "Login";
+            this.btnLogin.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.btnLogin.Timer_Effect_1 = 5;
+            this.btnLogin.Timer_RGB = 300;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1072, 518);
+            this.Controls.Add(this.airForm1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(112, 35);
+            this.Name = "frmLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "themeForm1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.airForm1.ResumeLayout(false);
+            this.nightPanel1.ResumeLayout(false);
+            this.nightPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel WorkingArea;
+        private ReaLTaiizor.Forms.AirForm airForm1;
+        private ReaLTaiizor.Controls.NightPanel nightPanel1;
+        private ReaLTaiizor.Controls.CyberTextBox tbUserName;
+        private ReaLTaiizor.Controls.CyberButton btnLogin;
+        private System.Windows.Forms.CheckBox chkRememberMe;
+        private ReaLTaiizor.Controls.CyberTextBox tbPassword;
+        private ReaLTaiizor.Controls.BigLabel bigLabel3;
+        private ReaLTaiizor.Controls.BigLabel bigLabel2;
+        private ReaLTaiizor.Controls.BigLabel bigLabel1;
+        private ReaLTaiizor.Controls.CrownLabel crownLabel1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ MediManage/Users/frmLogin.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MediManage_Buisness;
+
+namespace MediManage
+{
+    public partial class frmLogin : Form
+    {
+
+        bool GetFromRegister = false;
+        public frmLogin()
+        {
+            InitializeComponent();
+            Login_Load();
+            //EventArgs e = new EventArgs();
+            //airForm1.ParentForm.AcceptButton =
+            //       btnLogin;
+            //airForm1.ParentForm.CancelButton = airForm1.();
+
+            // It is temp code
+            clsGlobal.RememberUsernameAndPasswordInRegistry("", "");
+        }
+
+        private void Login_Load()
+        {
+            string UserName = "", Password = "";
+
+            if (clsGlobal.GetStoredCredentialFromRegistry(ref UserName, ref Password))
+            {
+                tbUserName.TextButton = UserName;
+                tbPassword.TextButton = Password;
+                chkRememberMe.Checked = true;
+                tbUserName.ForeColor = Color.Black;
+                tbPassword.ForeColor = Color.Black;
+                tbPassword.Password = true;
+                GetFromRegister = true;
+            }
+            else
+                chkRememberMe.Checked = false;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {      
+            if (!this.ValidateChildren())
+            {
+                MessageBox.Show("Some fileds are not valid", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            clsUser user = new clsUser();
+
+            if (!GetFromRegister)
+            {
+                user = clsUser.FindByUsernameAndPassword(tbUserName.TextButton.Trim(), clsGlobal.ComputeHash(tbPassword.TextButton.Trim()));
+            }
+            else
+                user = clsUser.FindByUsernameAndPassword(tbUserName.TextButton.Trim(), tbPassword.TextButton.Trim());
+
+
+
+            // It is temp code
+            user = clsUser.FindByUsernameAndPassword(tbUserName.TextButton.Trim(), clsGlobal.ComputeHash(tbPassword.TextButton.Trim()));
+
+
+            if (user != null)
+            {
+
+                if (chkRememberMe.Checked)
+                {
+                    clsGlobal.RememberUsernameAndPasswordInRegistry(tbUserName.TextButton.Trim(), tbPassword.TextButton.Trim());
+                }
+                else
+                {
+                    clsGlobal.RememberUsernameAndPasswordInRegistry("", "");
+                }
+
+                //incase the user is not active
+                if (!user.IsActive.Value)
+                {
+
+                    tbUserName.Focus();
+                    MessageBox.Show("Your account is not Active, Contact Admin.", "In Active Account", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                clsGlobal.CurrentUser = user;
+                this.Hide();
+                Form frm = new frmMainScreen(this);
+                frm.ShowDialog();
+
+            }
+            else
+            {
+                tbUserName.Focus();
+                MessageBox.Show("Invalid Username/Password.", "Wrong Credintials", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tbUserName_Enter(object sender, EventArgs e)
+        {
+            if (tbUserName.TextButton == "User name")
+                tbUserName.textBox.Clear();
+            tbUserName.ForeColor = Color.Black;
+        }
+
+        private void tbPassword_Enter(object sender, EventArgs e)
+        {
+            if (tbPassword.TextButton == "Password")
+            {
+                tbPassword.textBox.Clear();
+                tbPassword.Password = true;
+            }
+            tbPassword.ForeColor = Color.Black;
+
+            //GetFromRegister = true;
+        }
+
+        private void tbPassword_Leave(object sender, EventArgs e)
+        {
+            if (tbPassword.TextButton == "" || tbPassword.TextButton == null)
+            {
+                tbPassword.ForeColor = Color.Gray;
+                tbPassword.TextButton = "Password";
+                tbPassword.Password = false;
+            }
+        }
+
+        private void tbUserName_Leave(object sender, EventArgs e)
+        {
+            if (tbUserName.TextButton == "" || tbPassword.TextButton == null)
+            {
+                tbUserName.ForeColor = Color.Gray;
+                tbUserName.TextButton = "User name";
+            }
+        }
+
+        private void tbUserName_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbUserName.TextButton))
+            {
+                e.Cancel = true;
+                tbUserName.Focus();
+                errorProvider1.SetError(tbUserName, "UserName should have a value");
+            }
+        }
+
+        private void tbPassword_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbPassword.TextButton))
+            {
+                e.Cancel = true;
+                tbPassword.Focus();
+                errorProvider1.SetError(tbPassword, "Password should have a value");
+            }
+        }
+
+    }
+}
